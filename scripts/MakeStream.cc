58e3fdee437132879a09d9a0827d269b442dc984
Georg Auzinger <georg.auzinger@cern.ch>
2020-04-17 15:59:45 +0200

    working commit

--- scripts/MakeStream.cc
+++ scripts/MakeStream.cc
@@ -10,46 +10,6 @@
 
 using namespace std;
 
-//IntMatrix empty_matrix(uint32_t nrows, uint32_t ncols){
-//IntMatrix matrix;
-//matrix.reserve(nrows);
-//for (uint32_t r = 0; r < nrows; r++) {
-//vector<ADC> row;
-//row.reserve(ncols);
-//for (uint32_t c = 0; c < ncols; c++) {
-//row.push_back(0);
-//}
-//assert(row.size()==ncols);
-//matrix.push_back(row);
-//}
-//assert(matrix.size()==nrows);
-//return matrix;
-//}
-
-//IntMatrix getChipMatrix(uint32_t chip, IntMatrix theMatrix)
-//{
-
-//}
-
-/**
- * Convert a row and column address from sensor (1344x432) to module coordinates (672x864).
- */
-//void convert_address(uint32_t& row, uint32_t& col)
-//{
-//if(row & 1 == 0)//this is a simple check if row is an even number, alternative (row%2 == 0)
-//{
-//row/=2;
-//col *=2;
-//col+=1;
-//}
-//else// odd row number
-//{
-//row-=1;
-//row/=2;
-//col*=2;
-//}
-//}
-
 
 /**
  * Write a vector of bits to the stream output file.
@@ -57,7 +17,7 @@ using namespace std;
  * The stream is formatted into AURORA blocks of 64 bits length.
  * Each block is written into a new line of the output file.
  */
-void write_bits(ofstream & f, vector<bool> bits, int & bits_written) {
+void write_bits(std::ofstream & f, std::vector<bool> bits, int & bits_written) {
     for (auto bit: bits){
         if((bits_written % 64 == 0)){
             // Start new stream
@@ -76,8 +36,8 @@ void write_bits(ofstream & f, vector<bool> bits, int & bits_written) {
 /**
  * Convenience overload to write a single bit in the same manner.
  */
-void write_bits(ofstream & f, bool bit, int & bits_written) {
-    vector<bool> vec = {bit};
+void write_bits(std::ofstream & f, bool bit, int & bits_written) {
+    std::vector<bool> vec = {bit};
     write_bits(f, vec, bits_written);
 }
 
@@ -85,12 +45,12 @@ void write_bits(ofstream & f, bool bit, int & bits_written) {
 /**
  * Given a set of QCores, write the formatted data stream to file.
  */
-void stream_to_file(vector<QCore> & qcores, bool tot){
-    ofstream f;
+void stream_to_file(std::vector<QCore> & qcores, bool tot){
+    std::ofstream f;
     f.open("stream.txt");
     bool new_ccol = true;
     int bits_written = 0;
-    vector<bool> event = {1,0,1,0,1,0,0,1};
+    std::vector<bool> event = {1,0,1,0,1,0,0,1};
     write_bits(f, event, bits_written);
     f<<"|";
     int nqcore=0;
@@ -98,7 +58,7 @@ void stream_to_file(vector<QCore> & qcores, bool tot){
         int n1=0;
         int n2=0;
         if(new_ccol){
-            vector<bool> ccol_address = int_to_binary(53, 6);
+            std::vector<bool> ccol_address = int_to_binary(53, 6);
             write_bits(f, ccol_address, bits_written);
             // assert(ccol_address.size()==6);
         }
@@ -107,7 +67,7 @@ void stream_to_file(vector<QCore> & qcores, bool tot){
         write_bits(f, q.isneighbour, bits_written);
         f<<":";
         if(not q.isneighbour){
-            vector<bool> qrow_address = int_to_binary(q.qcrow, 8);
+            std::vector<bool> qrow_address = int_to_binary(q.qcrow, 8);
             write_bits(f, qrow_address, bits_written);
         }
         f<<":";
@@ -155,10 +115,10 @@ int main(int argc, char *argv[]){
     uint32_t nevent = 0;
 
     // 2D matrices of pixel ADCs, key == chip identifier
-    map<ChipIdentifier, IntMatrix> module_matrices;
-    map<ChipIdentifier, IntMatrix> chip_matrices;
+    std::map<ChipIdentifier, IntMatrix> module_matrices;
+    std::map<ChipIdentifier, IntMatrix> chip_matrices;
     // QCore objects per module, key == chip identifier
-    map<ChipIdentifier, vector<QCore>> qcores;
+    std::map<ChipIdentifier, std::vector<QCore>> qcores;
 
     //IntMatrix matrix;
     Encoder enc;
