df2927cb9008e83f44bd072dae7dbafb882673c0
Georg Auzinger <georg.auzinger@cern.ch>
2020-04-23 13:22:09 +0200

    added some simple histogramming for bandwidth estimation

--- scripts/MakeStream.cc
+++ scripts/MakeStream.cc
@@ -2,6 +2,8 @@
 #include <TFile.h>
 #include <TTreeReader.h>
 #include <TTreeReaderArray.h>
+#include <TCanvas.h>
+#include <TH1F.h>
 //#include <include/cxxopts.hpp>
 #include <util/Util.h>
 #include <util/IntMatrix.h>
@@ -37,18 +39,37 @@ int main (int argc, char* argv[])
 
     uint32_t nrows_module = 672;
     uint32_t ncols_module = 864;
-    int nevents = 0;
+    int nevents = 10;
 
     uint32_t nevent = 0;
 
+    //create one binary data file per DTC - 5 DTC's per side: TEPX D1 & D2 far: DTC1, TEPX D3 & D4 far: DTC2, TEPX D1 & D2 near: DTC3,...
+    //D4R1 near & far: DTC5
+
     std::map<uint32_t, std::ofstream*> outfile_map;
+    std::map<uint32_t, TH1F*> eventSizeDTCbits;
+    std::map<uint32_t, size_t> WordCountDTC;
+    TH1F* eventSizeChipbits = new TH1F ("streamsizeChipsbits", "Distribution of Chip Event sizes [bits]; bits; counts", 30, 0, 1920);
+    TH1F* eventSizeChipwords = new TH1F ("streamsizeChipswords", "Distribution of Chip Event sizes [16 bit words]; words; counts", 128, 0, 512);
+    //std::map<uint32_t, TH1F*> eventSizeDTC;
 
     for (uint32_t dtc = 1; dtc <= 5; dtc++)
     {
         std::stringstream ss;
         ss << "dtc_" << dtc << "_data.dat";
-        std::ofstream stream (ss.str(), std::ios::out | std::ios::binary);
-        outfile_map[dtc] = &stream;
+        //std::ofstream stream (ss.str(), std::ios::out | std::ios::binary);
+        outfile_map[dtc] = new std::ofstream (ss.str().c_str(), std::ios::out | std::ios::binary);
+
+        if (!outfile_map[dtc] || !*outfile_map[dtc])
+            perror (ss.str().c_str() );
+
+        //the perDTC histograms
+        std::stringstream namestream;
+        std::stringstream titlestream;
+        namestream << "EventSizeDTC" << dtc;
+        titlestream << "Event Size for DTC " << dtc << ";16 bit words;count";
+
+        eventSizeDTCbits[dtc] = new TH1F (namestream.str().c_str(), titlestream.str().c_str(), 100, 0, 30000);
     }
 
     // Event loop
@@ -56,6 +77,10 @@ int main (int argc, char* argv[])
     {
         if (nevent > nevents)
             break;
+
+        //clear the word counter for the DTCs for each event
+        for (uint32_t dtc = 1; dtc <= 5; dtc++)
+            WordCountDTC[dtc] = 0;
 
         uint32_t ientry = 0;
 
@@ -97,26 +122,24 @@ int main (int argc, char* argv[])
                 module_matrices.emplace (tmp_id, tmp_matrix);
             }
 
-            //have a map of map<DTC, vector<T>> where vector is the list of encoded modules
-
             //in all other cases the Intmatrix for that module exists already
+
             //get the row, col and ADC (attention, sensor row and column address) for the current module
             uint32_t row = trv_row.At (ientry);
             uint32_t col = trv_col.At (ientry);
             uint32_t adc = trv_adc.At (ientry);
-            //some sanity checks
-            //convert to module address
+
+            //convert to module address (different row and column numbers because 50x50)
             module_matrices.at (tmp_id).convertPitch_andFill (row, col, adc);
             ientry++;
         }//end module loop
 
-        // Loop over modules and create qcores for each module
         std::cout << "Finished reading full data for Event " << nevent << " from the tree; found " << module_matrices.size() << " modules for TEPX" << std::endl;
 
-        //for each DTC, encode the module coordinates in a vector<uint16_t>, determine the size of that vector and create a file header for each DTC;
         //do all of the below only for the first event
         if (nevent == 0)
         {
+            //for each DTC, encode the module coordinates (disk,ring,moduleID, # of chips) in a vector<uint16_t>, determine the size of that vector and create a file header for each DTC: 1st word: data format version, 2nd word, size of the module list, then: 1 word per module;
             std::map<uint32_t, std::vector<uint16_t>> dtc_map;
 
             for (auto module : module_matrices)
@@ -128,14 +151,14 @@ int main (int argc, char* argv[])
                 uint16_t header_version = 1;
                 dtc.second.insert (dtc.second.begin(), dtc.second.size() );
                 dtc.second.insert (dtc.second.begin(), header_version );
-                std::cout << "Now the size is " << dtc.second.size() << " and the first element is " << dtc.second.at (0) << " and the header size is " << dtc.second.at (1) << std::endl;
+                std::cout << "Now the size is " << dtc.second.size() << " and the header version is " << dtc.second.at (0) << " and the header (module list) size is " << dtc.second.at (1) << std::endl;
                 //now to file
                 Serializer::to_file<uint16_t> (outfile_map[dtc.first], dtc.second);
             }
         }
 
 
-        //now split the tmp_matrix in 4 chip matrices, construct the Chip identifier object and insert into the matrices map
+        //now split the tmp_matrix  for each module in 4 chip matrices, construct the Chip identifier object and insert into the matrices map
         for (auto matrix : module_matrices)
         {
             for (uint32_t chip = 0; chip < 4; chip++)
@@ -150,8 +173,6 @@ int main (int argc, char* argv[])
 
         for (auto chip : chip_matrices )
         {
-            //Print out chip coordinates
-            //chip.first.print();
             //pick apart each chip int matrix in QCores
             std::vector<QCore> qcores = enc.qcores (chip.second, nevent, chip.first.mmodule, chip.first.mchip);
 
@@ -161,6 +182,14 @@ int main (int argc, char* argv[])
 
             std::vector<uint16_t> binary_vec = Serializer::toVec<uint16_t> (tmp);
             Serializer::to_file<uint16_t> (outfile_map[chip.first.mdtc], binary_vec);
+            //increment the wordcount for this DTC
+            WordCountDTC[chip.first.mdtc] += binary_vec.size();
+
+            //fill the histograms for the chip data size
+            eventSizeChipbits->Fill (binary_vec.at (0) *sizeof (uint16_t) * 8 ); //each binary vector is prepended with the number of words per chip in the typename
+            eventSizeChipwords->Fill (binary_vec.at (0) ); //each binary vector is prepended with the number of words per chip in the typename
+
+
 
             //std::cout << "Size in 64 bit words: " <<  tmp.size() / 64 << " in 16 bit words " << tmp.size() / 16 << " of the resulting binary vec (remember: +1 for the number of data words!) " << binary_vec.size() << std::endl;
 
@@ -169,9 +198,31 @@ int main (int argc, char* argv[])
             //std::cout << ss.str() << std::endl;
         }
 
+        for (uint32_t dtc = 1; dtc <= 5; dtc++)
+        {
+            eventSizeDTCbits[dtc]->Fill (WordCountDTC[dtc]);
+            std::cout << "Event: " << nevent << " DTC: " << dtc << " has a word count (16bit) of " << WordCountDTC[dtc] << std::endl;
+        }
+
         nevent++;
     }//end event loop
 
+    TCanvas* canvas = new TCanvas();
+    canvas->Divide (2, 4);
+    canvas->cd (1);
+    eventSizeChipbits->Draw();
+    canvas->cd (2);
+    eventSizeChipwords->Draw();
+
+    //close the files
     for (auto& file : outfile_map)
+    {
         file.second->close();
+        delete file.second;
+        canvas->cd (file.first + 2);
+        eventSizeDTCbits[file.first]->Draw();
+    }
+
+    canvas->SaveAs ("Bandwidth.pdf");
+    canvas->SaveAs ("Bandwidth.root");
 }
